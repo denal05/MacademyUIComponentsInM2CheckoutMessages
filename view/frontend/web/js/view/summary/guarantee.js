8554542df4568259884d66203ca8835878f6b56a
Denis Aleksandrov <denal05@gmail.com>
2025-11-03 23:23:58 +0100

    Update guarantee.js
    Add initialize function to guarantee

--- view/frontend/web/js/view/summary/guarantee.js
+++ view/frontend/web/js/view/summary/guarantee.js
@@ -2,6 +2,10 @@ define(['uiComponent'], function(Component) {
     'use strict';
     
     return Component.extend({
+        initialize: function() {
+            this._super();
+            console.log(this.name + ' is initialized.';
+        },
         showMessage: function() {
             return this.subtotal > 100;
         }
