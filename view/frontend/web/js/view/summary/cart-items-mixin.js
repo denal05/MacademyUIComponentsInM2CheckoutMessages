51b498770f47bcaaab4ea97cc906357591a6ad2d
Denis Aleksandrov <denal05@gmail.com>
2025-11-03 21:53:24 +0100

    Update cart-items-mixin.js
    Add exports: 'totals.subtotal': 'checkout.sidebar.guarantee:subtotal'

--- view/frontend/web/js/view/summary/cart-items-mixin.js
+++ view/frontend/web/js/view/summary/cart-items-mixin.js
@@ -5,6 +5,7 @@ define([], function() {
         return Component.extend({
             defaults: {
                 template: 'Denal05_MacademyUIComponentsInM2CheckoutMessages/summary/cart-items'
+                exports: 'totals.subtotal': 'checkout.sidebar.guarantee:subtotal'
             },
             isItemsBlockExpanded: function () {
                 return true;
