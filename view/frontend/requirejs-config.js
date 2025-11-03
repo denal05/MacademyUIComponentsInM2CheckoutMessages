81efa706635fb2d2c00f2395ff0e9f3d6925b75f
Denis Aleksandrov <denal05@gmail.com>
2025-11-03 13:54:55 +0100

    Update requirejs-config.js
    Override a UI Component template with static path mapping

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,5 +5,14 @@ var config = {
                 'Denal05_MacademyUIComponentsInM2CheckoutMessages/js/view/summary/cart-items-mixin': true
             }
         }
+    },
+    /*
+    Don't use a map to override a template unless absolutely necessary; instead, try to use a template property in your UI Component.
+    */
+    map: {
+        '*': {
+            'Magento_Checkout/template/sidebar':
+                'Denal05_MacademyUIComponentsInM2CheckoutMessages/template/sidebar'
+        }
     }
 }
