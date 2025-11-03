da3797ed0824d98a43f96acea9e3341f0b28200d
Denis Aleksandrov <denal05@gmail.com>
2025-11-03 10:08:48 +0100

    Create requirejs-config.js
    Override Magento_Checkout/js/view/summary/cart-items.js with a mixin.

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/summary/cart-items': {
+                'Denal05_MacademyUIComponentsInM2CheckoutMessages/js/view/summary/cart-items-mixin': true
+            }
+        }
+    }
+}
